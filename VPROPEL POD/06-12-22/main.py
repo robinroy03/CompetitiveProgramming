0e6058b56211efda28e29b962c4aef67426fdd74
Robin Roy <115863770+robinroy03@users.noreply.github.com>
2022-12-06 20:48:11 +0530

    Update main.py

--- VPROPEL POD/06-12-22/main.py
+++ VPROPEL POD/06-12-22/main.py
@@ -1,15 +1,15 @@
 # --- accepting inputs from system ---
 
-m = int(input()) # maximum prize amount
-n = int(input()) # number of players
-l = [] # [['A', 2], ['B', 3] ... ]
+m = int(input())    # maximum prize amount
+n = int(input())    # number of players
+l = []              # [['A', 2], ['B', 3] ... ]
 
 for _ in range(n):
     temp = input().split()
     l.append([temp[0], int(temp[1])])
 
-first = int(input()) # index of the guy who caught it first - 100% prize
-x = input() # prize of the guy to be found - his name given
+first = int(input())    # index of the guy who caught it first - 100% prize
+x = input()             # prize of the guy to be found - his name given
 
 # --- all inputs received ---
 
@@ -23,6 +23,6 @@ while True:
     if name == x:
         break
     else:
-        perc-=2
+        perc -= 2
 
 print("{:.2f}".format(m*(perc/100)))
