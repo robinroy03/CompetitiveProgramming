c36cd2ae2c9b846b4dcb2ac004ec4df90d080d40
Robin Roy <115863770+robinroy03@users.noreply.github.com>
2022-12-11 21:12:27 +0530

    Update main.py

--- VPROPEL POD/11-12-22/main.py
+++ VPROPEL POD/11-12-22/main.py
@@ -2,16 +2,13 @@ a, b = map(int,input().split())     # intial price day 1 2
 q = int(input())                    # number of queries
 l = list(map(int,input().split()))  # q space sep integers
 
-k=[a,b]
+print(a,b,end=' ')
 for i in range(3,max(l)+1):
     c = 3*b + 4*a
     if i in l:
-        k.append(c)
+        if c > 1000000007:
+            print(c%1000000007,end=' ')
+        else:
+            print(c,end=' ')
     a = b
     b = c
-    
-for i in k:
-    if i > 1000000007:
-        print(i%1000000007,end=' ')
-    else:
-        print(i,end=' ')
