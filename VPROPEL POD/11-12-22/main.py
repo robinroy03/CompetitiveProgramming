97ee685d4cb91fc93e4c7b324b2c131d48709479
Robin Roy <115863770+robinroy03@users.noreply.github.com>
2022-12-11 23:31:15 +0530

    Update main.py

--- VPROPEL POD/11-12-22/main.py
+++ VPROPEL POD/11-12-22/main.py
@@ -6,8 +6,5 @@ print(a,b,end=' ')
 for i in range(3,max(l)+1):
     c = 3*b + 4*a
     if i in l:
-        if c > 1000000007:
-            print(c%1000000007,end=' ')
-        else:
-            print(c,end=' ')
+        print(c%1000000007,end=' ')
     a, b = b, c
