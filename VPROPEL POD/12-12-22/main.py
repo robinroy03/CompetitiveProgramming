742994cbd7a0cad807d6b7cf0089275632650e55
Robin Roy <115863770+robinroy03@users.noreply.github.com>
2022-12-13 18:23:26 +0530

    Update main.py

--- VPROPEL POD/12-12-22/main.py
+++ VPROPEL POD/12-12-22/main.py
@@ -1,14 +1,13 @@
-from itertools import repeat
 from math import factorial
 
 l = [int(input()) for i in range(int(input()))]
 
 for i in l:
-    initial = list(repeat(1,i))     # initial 1 filled list
+    initial = [1]*i     # initial 1 filled list
     value = 1
     while initial.count(1) >= 2:
         initial.remove(1)
         initial.remove(1)
         initial.append(2)
         value = value + (factorial(len(initial))//(factorial(initial.count(1))*factorial(initial.count(2))))
-    print(value % (10**9+7))
+    print(value%(10**9+7))
