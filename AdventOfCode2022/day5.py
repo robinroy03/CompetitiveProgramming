a4c7726b2831c54cf2a02e23b66099e820c45b93
Robin Roy <115863770+robinroy03@users.noreply.github.com>
2022-12-23 18:23:23 +0530

    Update day5.py

--- AdventOfCode2022/day5.py
+++ AdventOfCode2022/day5.py
@@ -2,7 +2,8 @@ import copy
 
 # hardcoding the dictionary
 
-d={1:['s','m','r','n','w','j','v','t'],
+d = {
+    1:['s','m','r','n','w','j','v','t'],
     2:['b','w','d','j','q','p','c','v'],
     3:['b','j','f','h','d','r','p'],
     4:['f','r','p','b','m','n','d'],
@@ -13,18 +14,22 @@ d={1:['s','m','r','n','w','j','v','t'],
     9:['l','s','g']
 }
 
+d2 = copy.deepcopy(d) # for part 2
 
-d2=copy.deepcopy(d) # for part 2
-
-f=open("day5_input.txt",'r')
-l=f.readlines()
+f = open("day5_input.txt",'r')
+l = f.readlines()
 f.close()
 
 def instructions(line):
-    cmd=line.rstrip().split()
-    count,fro,to=int(cmd[1]),int(cmd[3]),int(cmd[5])
+    
+    cmd = line.rstrip().split()
+    count,fro,to = int(cmd[1]),int(cmd[3]),int(cmd[5])
+    temp = []
+
     for i in range(count):
         d[to].append(d[fro].pop())
+        temp.append(d2[fro].pop())
+    d2[to].extend(reversed(temp))
 
 for line in l:
     instructions(line)
@@ -33,18 +38,5 @@ for i in d:
     print(d[i][-1],end="")  # part 1
 print()
 
-# ---- part 2 begins ---- 
-
-def instructions_2(line):
-    cmd=line.rstrip().split()
-    count,fro,to=int(cmd[1]),int(cmd[3]),int(cmd[5])
-    temp=[]
-    for i in range(count):
-        temp.append(d2[fro].pop())
-    d2[to].extend(reversed(temp))
-
-for line in l:
-    instructions_2(line)
-
-for i in d:
+for i in d2:
     print(d2[i][-1],end="") # part 2
