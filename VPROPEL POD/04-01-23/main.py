fc9f5c2f99e60f9e618f8c1861df4c5411802d0d
Robin Roy <115863770+robinroy03@users.noreply.github.com>
2023-01-04 21:53:29 +0530

    Create main.py

---
+++ VPROPEL POD/04-01-23/main.py
@@ -0,0 +1,40 @@
+k = int(input())
+m = int(input())
+l = list(map(int,input().split()))
+
+l2 = list(set(l))
+l2.sort(reverse = True)
+dic = {}
+v = l2.copy()           # this stores the final value
+
+for i in l2:
+    if l.count(i) % (i+1) == 0:
+        dic[i] = l.count(i) - (l.count(i)//(i+1)) - 1
+    else:
+        dic[i] = l.count(i) - ((l.count(i)//(i+1)) + 1) - 1
+
+while len(v) < k:
+    for i in l2:
+        if dic[i] > 0 and len(v) < k:
+            v.append(i)
+            dic[i] -= 1
+            
+print(sum(v) + k)
+
+
+"""
+TL;DR
+
+A lotta ngo people send up requests to an mnc. Multiple people from same ngo
+could apparently send reqs too. 
+When they send req they send how many are in the group "excluding" them.
+
+eg: dude say's 3 --> he means there are 4 people in the group including him
+
+when 4 people say 3 --> it means 4 people "might" be from same ngo coz
+when group of 4, they send 3... uhh.. got it?
+
+If you got it you're done with understanding question.
+
+Now you gotta relax and solve it.
+"""
