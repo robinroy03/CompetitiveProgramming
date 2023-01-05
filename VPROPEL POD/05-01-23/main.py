ed16bdf50b4d1985e6670ea157541d1b71c887a5
Robin Roy <115863770+robinroy03@users.noreply.github.com>
2023-01-05 20:18:13 +0530

    Update main.py

--- VPROPEL POD/05-01-23/main.py
+++ VPROPEL POD/05-01-23/main.py
@@ -7,12 +7,12 @@ b = int(input())
 time = n*(min(a,b))    # time required when fastest typer is doing solo
 
 for i in range(0,n+1):
-    ta=a*i          # time required for typist 'a' to type 'i' words
-    tb=b*(n-i)      # time required for typist 'b' to type '(n-i)' words
+    ta = a*i          # time required for typist 'a' to type 'i' words
+    tb = b*(n-i)      # time required for typist 'b' to type '(n-i)' words
     
-    temp=max(ta,tb) # new time is the time taken by slowest typist to complete
+    temp = max(ta,tb) # new time is the time taken by slowest typist to complete
     
-    if temp<time:   
-        time=temp
+    if temp < time:   
+        time = temp
 
 print(time)
